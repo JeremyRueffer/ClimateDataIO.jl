3cd9796b603b3ecdab095ab3f7dd82745911eaee
Jeremy <jeremy.rueffer@thuenen.de>
2016-12-14 13:52:48 +0100

    Aerodyne tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,23 +5,52 @@ using Base.Test
 src = splitdir(@__FILE__)[1]
 mindate = DateTime(2016,11,23,14,4)
 maxdate = DateTime(2016,11,23,18)
-Data = ClimateDataIO.sltload(src,mindate,maxdate)
+Data = ClimateDataIO.sltload(src,mindate,maxdate,verbose=true)
 
-@test size(Data) == (141377,13) || "Output size incorrect, should be (141377,13)"
+@test size(Data) == (141377,13) || "SLTLOAD: Output size incorrect, should be (141377,13)"
 
-@test Data[:Time][1] == DateTime(2016,11,23,14,4) || "First timestamp should be 2016-11-23T14:04:00"
+@test Data[:Time][1] == DateTime(2016,11,23,14,4) || "SLTLOAD: First timestamp should be 2016-11-23T14:04:00"
 
-@test Data[:Time][end] == DateTime(2016,11,23,17,59,59,900) || "Last timestamp should be 2016-11-23T17:59:59.9"
+@test Data[:Time][end] == DateTime(2016,11,23,17,59,59,900) || "SLTLOAD: Last timestamp should be 2016-11-23T17:59:59.9"
 
 # SLTREAD: Load one known file
 src = splitdir(@__FILE__)[1]
 src = joinpath(src,"W20163281000.slt")
 analog_inputs = 6 # Number of analog inputs
 sample_frequency = 10 # Hz
-Time,Data = sltread(src,analog_inputs,sample_frequency)
+Time,Data = ClimateDataIO.sltread(src,analog_inputs,sample_frequency)
 
-@test size(Data) == (18003,10) || "Output size incorrect, should be (18003,10)"
+@test size(Data) == (18003,10) || "SLTREAD: Output size incorrect, should be (18003,10)"
 
-@test Time[1] == DateTime(2016,11,23,10) || "First timestamp should be 2016-11-23T10:00:00"
+@test Time[1] == DateTime(2016,11,23,10) || "SLTREAD: First timestamp should be 2016-11-23T10:00:00"
 
-@test Time[end] == DateTime(2016,11,23,10,30,00,200) || "Last timestamp should be 2016-11-23T10:30:00.2"
+@test Time[end] == DateTime(2016,11,23,10,30,00,200) || "SLTREAD: Last timestamp should be 2016-11-23T10:30:00.2"
+
+# AERODYNE_LOAD: Load one file
+src = splitdir(@__FILE__)[1]
+src = joinpath(src,"161210_000000.str")
+Time,Data = ClimateDataIO.aerodyne_load(src,verbose=true)
+
+@test Time[1] == DateTime(2016,12,10,0,0,0,622) || "AERODYNE_LOAD: First timestamp should be 2016-12-10T00:00:00.622"
+
+@test Time[end] == DateTime(2016,12,10,0,59,59,922) || "AERODYNE_LOAD: Last timestamp should be 2016-12-10T00:59:59.922"
+
+@test size(Data) == (35159,2) || "AERODYNE_LOAD: Output size incorrect, should be (35159,2)"
+
+# AERODYNE_LOAD: Load all file
+src = splitdir(@__FILE__)[1]
+TimeStr,DataStr,TimeStc,DataStc = ClimateDataIO.aerodyne_load(src,verbose=true)
+
+@test TimeStr[1] == DateTime(2016,12,10,0,0,0,622) || "AERODYNE_LOAD: TimeStr first timestamp should be 2016-12-10T00:00:00.622"
+
+@test TimeStr[end] == DateTime(2016,12,10,3,59,59,917) || "AERODYNE_LOAD: TimeStr last timestamp should be 2016-12-10T03:59:59.917"
+
+@test size(DataStr) == (140641,2) || "AERODYNE_LOAD: DataStr output size incorrect, should be (140641,2)"
+
+@test TimeStc[1] == DateTime(2016,12,10,0,0,2,622) || "AERODYNE_LOAD: TimeStc first timestamp should be 2016-12-10T00:00:02.622"
+
+@test TimeStc[end] == DateTime(2016,12,10,3,59,59,117) || "AERODYNE_LOAD: TimeStc last timestamp should be 2016-12-10T03:59:59.117"
+
+@test size(DataStc) == (14391,26) || "AERODYNE_LOAD: DataStc output size incorrect, should be (14391,26)"
+
+println("All Tests Complete Successfully")
